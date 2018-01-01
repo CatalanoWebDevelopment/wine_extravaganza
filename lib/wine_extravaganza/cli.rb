60144da27938af012554832f72543d87edefdbcf
catalanowebdevelopment <catalano.work@gmail.com>
2018-01-01 11:28:59 -0500

    Updated CLI

--- lib/wine_extravaganza/cli.rb
+++ lib/wine_extravaganza/cli.rb
@@ -4,17 +4,30 @@ require 'pry'
 class WineExtravaganza::CLI
     
     def call
-        puts "Welcome to WineExtravaganza! To see a list of the most current, top-rated wines, type in Merlot, Cab, Chardonnay, Pinot noir, Pinot gris, Malbec, Sauvignon blanc, or Riesling for a description of the blend and the top ten highest values. To quit the program, just type 'exit'. Enjoy!".blue
-        puts "-------------------------------------------------".white
-        puts "Which wine would you like to look at?".blue
-        
-        # SEPERATE METHODS RUNNING
         list_deals
+        menu
     end
     
     def list_deals
-        input = gets.strip.downcase
-        binding.pry
+       puts "Welcome to WineExtravaganza! To see a list of the most current, top-rated wines, type in the number associated with Merlot, Cabernet, Chardonnay, Pinot noir, Pinot gris, Malbec, Sauvignon blanc, or Riesling for a description of the blend and the top ten highest values. To quit the program, just type 'exit'. Enjoy!".blue
+       puts "-------------------------------------------------".white
+       puts "Which wine would you like to look at?".blue
+    
+        puts "1. Merlot".red
+        puts "2. Cabernet".blue
+        puts "3. Chardonnay".red
+        puts "4. Pinot noir".blue
+        puts "5. Pinot gris".red
+        puts "6. Malbec".blue
+        puts "7. Sauvignon blanc".red
+        puts "8. Riesling".blue
+        puts "-------------------------------------------------".white
+
+    end
+    
+    def menu
+        input = gets.strip.to_i
+    
     end
     
 end
